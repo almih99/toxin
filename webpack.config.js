aa3cb29be1ccb6ff50b1918ddb409b14090c4484
almih99 <almih99@mail.ru>
2019-04-20 19:52:25 +0300

    Pug file transformation was configured

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,53 @@
 const path=require("path");
 
+
 const config={
-  entry: './src/index.js',
+  entry: {
+   index: './src/test.pug'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-    publicPath: './dist/'
+    filename: '[name].js'
   },
   devServer: {
     overlay: true
+  },
+  module: {
+    rules: [
+      // transform pug files
+      {
+        test: /\.pug$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].html'
+            }
+          },
+          {
+            loader: 'extract-loader'
+          },
+          {
+            loader: 'html-loader',
+          },
+          {
+            loader: 'pug-html-loader',
+            options: {
+              pretty: true,
+              globas: ['require']
+            }
+          }
+        ]
+      },
+      // copy files
+      {
+        test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/, loader: 'file-loader',
+        options: {
+          name: 'img/[name].[ext]'
+        }
+      }
+    ]
   }
-  
 }
 
 module.exports=config;
