79b82c7e7f2864dfaf738e47a0c2d3bceb593167
almih99 <almih99@mail.ru>
2019-05-13 15:44:21 +0300

    slider was refactored

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,10 +1,25 @@
-$(document).ready(
 
+const nf = new Intl.NumberFormat('ru-RU');
+
+function formatPriceRange(min, max) {
+    return nf.format(min) + "₽ - " + nf.format(max) + "₽";
+}
+
+function onSlide(e, ui) {
+    $(this)
+        .closest(".slider")
+        .find(".slider__label")
+        .text(formatPriceRange(ui.values[0],ui.values[1]));
+    var inputs = $(this)
+        .closest(".slider")
+        .find("input[type='hidden']")
+        .toArray();
+    inputs[0].value=ui.values[0];
+    inputs[1].value=ui.values[1];
+}
+
+$(document).ready(
 function registerAllSliders(e) {
-    const nf = new Intl.NumberFormat('ru-RU');
-    function formatPriceRange(min, max) {
-        return nf.format(min) + "₽ - " + nf.format(max) + "₽";
-    }
     $(".slider").each(
         function() {
             const min=Number($(this).attr("data-min")||0);
@@ -18,25 +33,13 @@ function registerAllSliders(e) {
                     max,
                     min,
                     values: [val1, val2],
-                    slide : function(event, ui) {
-                        $(this)
-                            .closest(".slider")
-                            .find(".slider__label")
-                            .text(formatPriceRange(ui.values[0],ui.values[1]));
-                        $(this)
-                            .closest(".slider")
-                            .attr("data-val1", ui.values[0])
-                            .attr("data-val2", ui.values[1]);
-                    }
+                    slide : onSlide
                 }
             );
-            $(this).find(".slider__label")
-                .text(formatPriceRange(val1, val2));
+            // simulation of event resault for initialization purpos
+            onSlide.call(this, null, {values: [val1, val2]});
         }
     )
 }
 
 ); // $(document).ready
-
-
-module.exports=null;
